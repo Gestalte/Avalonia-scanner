5b43a4f34d4701a4da16b5a49c79a24c759cad00
Gestalte <66849891+Gestalte@users.noreply.github.com>
2025-03-19 17:41:12 +0200

    Scan UI

--- AvaloniaApplication/ViewModels/MainViewModel.cs
+++ AvaloniaApplication/ViewModels/MainViewModel.cs
@@ -1,6 +1,18 @@
-﻿namespace AvaloniaApplication.ViewModels;
+﻿using ReactiveUI;
+
+namespace AvaloniaApplication.ViewModels;
 
 public class MainViewModel : ViewModelBase
 {
-    public string Greeting => "Welcome to Avalonia!";
+    private string scanResult = "";
+    public string ScanResult
+    {
+        get => scanResult;
+        set => scanResult = this.RaiseAndSetIfChanged(ref scanResult, value);
+    }
+
+    public void StartScanning()
+    {
+
+    }
 }
